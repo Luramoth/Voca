bbd99e78f2911b38052e71c8dc07b5eeb10ba78e
LuraMoth <fasreaperearnest@gmail.com>
2021-11-21 21:40:59 -0500

    make CL prints/warnings

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,12 @@
 //use core::num;
-use std::io;
-use std::cmp::Ordering;
-use rand::Rng;
+//use std::io;
+//use std::cmp::Ordering;
+//use rand::Rng;
 use colored::*;
 
 fn main (){
-    println!("fuck torvalds I swear to god I hate git and everyhitng it stands for");
-
-    let number = rand::thread_rng().gen_range(1, 10);
-
-    loop {
-        println!("now guess a number from 1-10 for no reason lol, psst, its {} btw", number);
-
-        let mut guess = String::new();
-    
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-    
-        let guess:u32  = match guess.trim().parse(){
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-
-        print!("you guessed: {}", guess);
-    
-        match guess.cmp(&number) {
-            Ordering::Greater => println!("{}", " and it was too big dummy".red()),
-            Ordering::Less => println!("{}", " and it was too small idiot".red()),
-            Ordering::Equal => {
-                println!("{}", " and it was correct".green());
-                break;
-            },
-        }
-    }
+    println!("Hello, welcome to {}", "Voca".green().bold());
+    println!("{}", "Please keep in mind this is a largely experimental program and its constantly in progress,".red().italic().bold());
+    println!("{}", "wether or not it will stay up to date or will be worked on is entirly possible to change.".red().italic().bold());
+    println!("{}", "This project is protected under the GPLv3 license and comes without warrenty.".purple().italic());
 }
