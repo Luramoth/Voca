4e974b306734b5e0db9ed80e483b74033eff3d28
LuraMoth <fasreaperearnest@gmail.com>
2021-12-29 11:57:03 -0500

    set up project as executable and spellcheck

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,9 @@
 use colored::*;
 
 fn main (){
-    println!("Hello, welcome to {}", "Voca".green().bold());
-    println!("{}", "Please keep in mind this is a largely experimental program and its constantly in progress,".red().italic().bold());
-    println!("{}", "wether or not it will stay up to date or will be worked on is entirly possible to change.".red().italic().bold());
-    println!("{}", "This project is protected under the GPLv3 license and comes without warrenty.".purple().italic());
+	println!("Hello, welcome to {}", "Voca".green().bold());
+	println!("{}", "Please keep in mind this is a largely experimental program and its constantly in progress,".red().italic().bold());
+	println!("{}", "wether or not it will stay up to date or will be worked on is entirely possible to change.".red().italic().bold());
+	println!("{}", "This project is protected under the GPLv3 license and comes without warranty.".purple().italic());
+	
 }
